3a222381bf82c795d12ed008b90ab270d49918db
Pavel <TagievPavelF@gmail.com>
2022-07-28 10:17:57 +0300

    add lines

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 
 int main(int, char**) {
-	std::cout << "*" << std::endl;
+	std::cout << "*"    << std::endl;
+	std::cout << "**"   << std::endl;
+	std::cout << "***"  << std::endl;
+	std::cout << "****" << std::endl;
 	return 0;
 }
